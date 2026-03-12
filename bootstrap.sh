5191f048af9655cc3b958c1e6476ef4b55bea079
SATYAVARDHAN04 <svardhan278@gmail.com>
2026-03-12 11:46:13 +0530

    terraform

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 dnf install ansible -y
-ansible-pull -U https://github.com/SATYAVARDHAN04/ansible-roles-roboshop.git -e component=$1 -e env=$2 main.yaml
+ansible-pull -U https://github.com/SATYAVARDHAN04/ansible-roles-roboshop-tf.git -e component=$1 -e env=$2 main.yaml
